6fbd10efe7b412668d6f03581e5ea334fe1e7095
ragulreigns <ragulreigns02@gmail.com>
2024-11-19 23:59:00 +0530

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Get the branch name, whether we're in detached HEAD state or not
-export GIT_BRANCH=$(git symbolic-ref --short HEAD || git rev-parse --short HEAD)
-
-echo "Current branch inside script: $GIT_BRANCH"
-
-# Check the branch and take action accordingly
-if [[ $GIT_BRANCH == "dev" ]]; then
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
     chmod +x build.sh
     build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag my-react-app ragul11/dev
     docker push ragul11/dev
 
-elif [[ $GIT_BRANCH == "master" ]]; then
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
     chmod +x build.sh
     build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
