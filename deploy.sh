ec5d8115bb1ec8b0102d8556d2ea9164d0b1e13e
ragulreigns <ragulreigns02@gmail.com>
2024-11-19 23:24:19 +0530

    Fix branch detection for Jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "Current branch inside script: $GIT_BRANCH"
+# Use Jenkins' environment variable GIT_BRANCH
+export GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 
-# Ensure the branch includes origin/
-GIT_BRANCH="origin/$GIT_BRANCH"
+echo "Current branch inside script: $GIT_BRANCH"
 
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
     chmod +x build/build.sh
-    ./build.sh
+    build/build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag my-react-app ragul11/dev
     docker push ragul11/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     chmod +x build/build.sh
-    ./build.sh
+    build/build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag my-react-app ragul11/prod
     docker push ragul11/prod
