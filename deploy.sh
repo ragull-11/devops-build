f65554506171d5446d40c8316a96c9b2945e24ac
ragulreigns <ragulreigns02@gmail.com>
2024-11-19 14:39:20 +0530

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 docker run -itd -p 80:80 my-react-app
 docker login -u ragul11 -p Ragul@docker2
 
-if [[$GIT_BRANCH == "origin/dev"]]; then
+if [[$GIT_BRANCH == "dev"]]; then
     docker tag my-react-app ragul11/dev
     docker push ragul11/dev
 
-elif [[$GIT_BRANCH == "origin/master"]]; then
+elif [[$GIT_BRANCH == "master"]]; then
     docker tag my-react-app ragul11/prod 
     docker push ragul11/pro
 fi
