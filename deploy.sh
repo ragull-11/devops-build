1b2f60fa77f251a48b713b959700b9ba2fa7b4b0
ragulreigns <ragulreigns02@gmail.com>
2024-11-20 00:24:06 +0530

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-    chmod +x build.sh
-    build.sh
+    chmod +x ./build.sh
+    ./build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag my-react-app ragul11/dev
     docker push ragul11/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
-    chmod +x build.sh
-    build.sh
+    chmod +x ./build.sh
+    ./build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag my-react-app ragul11/prod
     docker push ragul11/prod
